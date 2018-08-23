b1969ac2773ee5fdf7b2617c1a0238dd21b39cfa
ShimmeringDeep <dprandazzo@gmail.com>
2018-08-22 19:14:48 -0500

    lol i'm dumb

---
+++ oauth.js
@@ -0,0 +1,24 @@
+var ids = {
+    facebook: {
+      clientID: '325649148172616',
+      clientSecret: '1d8e6907996b98c6c01a85220712a689',
+      callbackURL: 'https://intheband.heroku.com/auth/facebook/callback'
+    },
+    twitter: {
+      consumerKey: '7XSr2AQMuLen87UvwMGpyQEEV',
+      consumerSecret: 'wVoBBd52Ee3il2L7jLQmLVLOvERkyZaDT7sLhFjDU1lHaIhdLY',
+      callbackURL: "https://intheband.heroku.com/auth/twitter/callback"
+    },
+    github: {
+      clientID: '9de52fb3c733b26b5ac5',
+      clientSecret: '305d444f104365577842e7944673d78481fd50c4',
+      callbackURL: "https://intheband.heroku.com/auth/github/callback"
+    },
+    google: {
+      clientID: '147601512434-m3ud0e1h6m21icu5bkrlr62ofeufogpm.apps.googleusercontent.com',
+      clientSecret: 'ac9Q5RoV4sFbpIZyv8HIYEJP',
+      callbackURL: 'https://intheband.heroku.com/auth/google/callback'
+    }
+  };
+ 
+  module.exports = ids;
